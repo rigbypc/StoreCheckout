32f8323312f54a61aec28416cb8e3ba40069064a
Peter C Rigby <rigbypc@gmail.com>
2019-01-28 12:46:42 -0800

    nothing is under test, all objects are mocked, this is just for fun!

--- StoreCheckout/src/test/java/test/point/of/sale/TestSale.java
+++ StoreCheckout/src/test/java/test/point/of/sale/TestSale.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import org.mockito.InOrder;
 
 import point.of.sale.Display;
-import point.of.sale.FakeDisplay;
+import point.of.sale.HashStorage;
 import point.of.sale.Sale;
 
 import static org.mockito.Mockito.*;
@@ -24,11 +24,31 @@ public class TestSale {
 		Display display = mock(Display.class);
 		
 		Sale sale = new Sale(display);
+		
+		
 		sale.scan("1A");
 		InOrder inOrder = inOrder(display);
 		inOrder.verify(display).showLine("1A");
 		inOrder.verify(display).showLine("Milk, 3.99");
 		
+	}
+	
+	@Test
+	public void testMockStubFun() {
+		
+		//nothing is under test, all objects are mocked, this is just for fun!
+		
+		HashStorage hashStorage = mock(HashStorage.class);
+		
+		when(hashStorage.barcode("Select * from people")).thenReturn("Peter, Blah");
+		when(hashStorage.barcode("1A")).thenReturn("Milk, 10.99");
+		hashStorage.put("1A", "Milk, 3.99");
+		
+		System.out.println(hashStorage.barcode("Select * from people"));
+		System.out.println(hashStorage.barcode("1A"));
+		System.out.println(hashStorage.barcode("Hello World"));
+		
+		verify(hashStorage).put("1A", "Milk, 3.99");
 	}
 
 }
