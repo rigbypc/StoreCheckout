e25bf1e0d1f68098360c424494338add169f9347
Peter C Rigby <rigbypc@gmail.com>
2019-01-30 15:21:21 -0500

    A dumb stubbing example

--- StoreCheckout/src/test/java/mocking/examples/TestMocking.java
+++ StoreCheckout/src/test/java/mocking/examples/TestMocking.java
@@ -7,10 +7,29 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 import java.util.List;
+import java.util.Stack;
 
 
 public class TestMocking {
 
+	
+	@Test
+	public void testStack() {
+		//create a mocked stack
+		Stack stack = mock(Stack.class);
+		
+		//stub mockStack.pop with 3,2,1
+		when(stack.pop()).thenReturn("3", "2", "1", null);
+		
+		//assert that the mockito stubbing function works correctly (ie write three pop asserts)
+		assertEquals("3", stack.pop());
+		verify(stack).pop();
+		assertEquals("2", stack.pop());
+		assertEquals("1", stack.pop());
+		
+		assertEquals(null, stack.pop());
+	}
+	
 	@Test
 	public void test() {
 		List<String> mockList = mock(List.class);
