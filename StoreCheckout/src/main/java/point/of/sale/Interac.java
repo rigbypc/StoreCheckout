b773ef10b75e051c48a887dfcffe7517dd1841c9
Peter C Rigby <rigbypc@gmail.com>
2019-02-04 11:38:41 -0500

    Adding an interact client

---
+++ StoreCheckout/src/main/java/point/of/sale/Interac.java
@@ -0,0 +1,21 @@
+package point.of.sale;
+
+import java.util.ArrayList;
+
+public class Interac {
+
+	int channel;
+	public Interac(int channel) {
+		this.channel = channel; 
+	}
+	
+	public void pay(ArrayList<String> items) {
+		for (String item : items) {
+			System.out.printf("Will make call to interac payment on channel %d for %s",
+					item, channel);
+		}
+		
+		
+	}
+
+}
--- StoreCheckout/src/main/java/point/of/sale/Sale.java
+++ StoreCheckout/src/main/java/point/of/sale/Sale.java
@@ -1,23 +1,30 @@
 package point.of.sale;
 
+import java.util.ArrayList;
 
 public class Sale {
 	
 	HashStorage hashLookup;
 	Display display;
+	Interac interac;
+	ArrayList<String> items = new ArrayList<>();
 	
 	public Sale(Display display, HashStorage hashStorage) {
 		this.display = display;
 		this.hashLookup = hashStorage;
+		this.interac = new Interac(12);
 	}
 	
 	public Sale() {
+		//display
 		display = new ArtR56Display();
 		
 		//Storage, add the items in the store
 		hashLookup = new HashStorage();
 		hashLookup.put("1A", "Milk, 3.99");
 		hashLookup.put("2A", "Bread, 4.99");
+		
+		this.interac = new Interac(12);
 	}
 	
 	public void scan(String barcode) {
