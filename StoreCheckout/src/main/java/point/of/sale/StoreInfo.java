2de89801bb076bfaa39f4fe2522970d2c093e963
Peter C Rigby <rigbypc@gmail.com>
2019-02-04 15:26:38 -0500

    Single for StoreInfo

---
+++ StoreCheckout/src/main/java/point/of/sale/StoreInfo.java
@@ -0,0 +1,29 @@
+package point.of.sale;
+
+public class StoreInfo {
+
+	private static StoreInfo instance = null;
+	private String name = "No Name";
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private StoreInfo() {
+		
+	}
+	
+	public static StoreInfo getInstance() {
+		if(instance == null) {
+			instance = new StoreInfo();
+		}
+		
+		return instance;
+	}
+
+}
