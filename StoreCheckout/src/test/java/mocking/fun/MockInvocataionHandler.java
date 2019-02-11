2388b381d3cf252572e7e2f4dd6bbb21c30ca906
Peter C Rigby <rigbypc@gmail.com>
2019-02-11 15:12:00 -0500

    writing a verify function

--- StoreCheckout/src/test/java/mocking/fun/MockInvocataionHandler.java
+++ StoreCheckout/src/test/java/mocking/fun/MockInvocataionHandler.java
@@ -8,20 +8,26 @@ import static org.junit.Assert.*;
 
 public class MockInvocataionHandler implements InvocationHandler {
 	
+	HashMap<String, String> verifyMap = new HashMap<String, String>();
+	
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		System.out.println(proxy.getClass());
 		
+		System.out.println("Display methods call:");
+		
+		//name of method that was called
 		System.out.println(method.getName());
 		
+		//the list of args
 		for (Object object : args) {
 			System.out.println(object.toString());
+			verifyMap.put(method.getName(), object.toString());
 		}
 		return null;
 	}
 
 	public void verify(String method, String parameter) {
-		// TODO Auto-generated method stub
+		assertEquals(verifyMap.get(method), parameter);
 		
 	}
 	
