06759a95f42130521e254f06dbbad8756bc5b277
Peter C Rigby <rigbypc@gmail.com>
2019-03-04 15:24:22 -0500

    comments showing steps in migration

--- StoreCheckout/src/test/java/test/point/of/sale/TestDataMigration.java
+++ StoreCheckout/src/test/java/test/point/of/sale/TestDataMigration.java
@@ -19,10 +19,21 @@ public class TestDataMigration {
 		storage.put("1", "Milk, 3.99");
 		storage.put("2", "Beer, 10.99");
 		
+		//Forklift (mass migration)
+		
+		//Consistency Checking (includes incremental replication)
+		
+		//Shadow writes (writes to old and new datastore)
+		
+		//Shadow reads (checks that old and new datastores return the same value
+		
+		//Read and write from new datastore (get rid of old datastore)
+		
+		
 		Sale sale = new Sale(display, storage, interac);
 		
 		sale.scan("1");
-		
+					
 		verify(display).showLine("Milk, 3.99");
 	}
 
