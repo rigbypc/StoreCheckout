ace9eab6640d4800be29312b25859de0b244007e
Peter C Rigby <rigbypc@gmail.com>
2019-03-18 15:20:30 -0400

    logging fun

--- StoreCheckout/src/test/java/test/point/of/sale/TestLogging.java
+++ StoreCheckout/src/test/java/test/point/of/sale/TestLogging.java
@@ -9,9 +9,23 @@ import org.junit.Test;
 
 public class TestLogging {
 
-	@Test
+	private static Logger rootLogger = LogManager.getLogger();
+	private static Logger fileLogger = LogManager.getLogger("migration");
+	
+	//@Test
 	public void testRoot() {
-		
+	
+		rootLogger.debug("Debug");
+		rootLogger.info("Info");
+		rootLogger.error("Error");
+	}
+
+	@Test
+	public void testFile() {
+	
+		fileLogger.debug("Debug");
+		fileLogger.info("Info");
+		fileLogger.error("Error");
 	}
 
 }
