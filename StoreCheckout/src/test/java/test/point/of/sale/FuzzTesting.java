1198717f8d1d92f25dc9b9b09048998c07ba7fb4
Peter C Rigby <rigbypc@gmail.com>
2019-04-08 15:33:35 -0400

    Pick items in the normal range

--- StoreCheckout/src/test/java/test/point/of/sale/FuzzTesting.java
+++ StoreCheckout/src/test/java/test/point/of/sale/FuzzTesting.java
@@ -16,23 +16,34 @@ public class FuzzTesting {
 	@Test
 	public void testSale() {
 		
-		Interac interac = mock(Interac.class); 
-		Display display = mock(Display.class);
+		int iterations = 10;
 		
-		ArrayStorage storage =  new ArrayStorage();
-		storage.put("1", "Milk, 3.99");
-		storage.put("2", "Beer, 9.99");
+		for (int i = 0; i < iterations; i++) {
 				
-		Sale sale = new Sale(display, storage, interac);
-		
-		//(null pointer) Tests the corner case and finds a bug that should be fixed
-		//Integer rndBarcode = ThreadLocalRandom.current().nextInt(3, 1000+1);
-		
-		// Array index out of bounds
-		Integer rndBarcode = ThreadLocalRandom.current().nextInt(1000, 10000+1);
-		
-		sale.scan(rndBarcode.toString());
-		verify(display).showLine("Milk, 3.99");
-		
+			Interac interac = mock(Interac.class); 
+			Display display = mock(Display.class);
+			
+			int numItems = 3;
+			ArrayStorage storage =  new ArrayStorage();
+			storage.put("1", "Milk, 3.99");
+			storage.put("2", "Beer, 9.99");
+			storage.put("3", "Bread, 19.99");
+					
+			Sale sale = new Sale(display, storage, interac);
+			
+			//(null pointer) Tests the corner case and finds a bug that should be fixed
+			//Integer rndBarcode = ThreadLocalRandom.current().nextInt(3, 1000+1);
+			
+			// Array index out of bounds
+			//Integer rndBarcode = ThreadLocalRandom.current().nextInt(1000, 10000+1);
+			
+			//know the number of items in the test (within the normal range), multiple
+			// Want to make sure that in the normal range I don't get exceptions
+			Integer rndBarcode = ThreadLocalRandom.current().nextInt(1, numItems+1);
+			
+			sale.scan(rndBarcode.toString());
+			verify(display, times(3)).showLine(any(String.class));
+		}
+			
 	}
 }
